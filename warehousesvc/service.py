8dbd1e3a11392edf3e021d102b346c9d18920b67
Martin Hellspong <martin.hellspong@factor10.com>
2016-11-15 11:32:14 +0100

    Add warehousesvc

---
+++ warehousesvc/service.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask, request, jsonify, send_from_directory
+
+app = Flask(__name__)
+
+@app.route('/scripts/<path:path>')
+def send_script(path):
+    return send_from_directory('static/scripts', path)
+
+@app.route('/views/<path:path>')
+def send_view(path):
+    return send_from_directory('static/views', path)
+
+@app.route('/host')
+def host():
+    return jsonify(os.uname())
+
+@app.route('/name')
+def name():
+    return jsonify("Python " + os.sys.version)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
